49695aa62d4c28c969105e91011cd39eb849b408
rony steve <rymel067@uottawa.ca>
2023-03-20 11:07:40 -0400

    4eme commit

--- engine.php
+++ engine.php
@@ -12,6 +12,28 @@ if( isset($_SESSION['etat']) && isset($_SESSION['joueurEnCours']) && isset($_SES
 
 
 
+if(isset($_POST['reset'])){
+    resetJeu($etat, $joueurEnCours, $victoire);
+    $tableau;
+    for($i = 0; $i < 9; $i++){
+        $tableau[$i]=$etat[$i];
+    }
+    array_push($tableau, $victoire);
+    array_push($tableau, $joueurEnCours);
+    echo json_encode($tableau);
+    exit();
+}else{
+    jouerT($_POST['id'], $joueurEnCours, $etat, $victoire);
+    $tableau;
+    for($i = 0; $i < 9; $i++){
+        $tableau[$i]=$etat[$i];
+    }
+    array_push($tableau, $victoire);
+    array_push($tableau, $joueurEnCours);
+    echo json_encode($tableau);
+    exit();
+}
+
 
 
 
--- jeu.js
+++ jeu.js
@@ -9,4 +9,36 @@ const jouerBlock = (e) => {
   let req = {
     id: parseInt(idblock)
   };
+  
+  $.ajax({
+    type: 'POST',
+    url: "engine.php",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    data: jQuery.param(req),
+    success: function (response) {
+      let arr = JSON.parse(response);
+      console.log(arr);
+      draw(arr);
+
+      if (arr[9] == 1) {
+
+        // cette commande indique quel est le joueur qui a gagné
+        alert("FELICITATION Le gagnant est le joueur " );
+        resetJeu();
+        
+        //reset
+
+        blocks.forEach((c) => (c.textContent = ""));
+      }
+      else if (arr[9] === 2) {
+        alert("Match null RESSAYER!");
+        resetetat();
+        blocks.forEach((c) => (c.textContent = ""));
+      }
+    }
+
+  });
+
+
+};
 
