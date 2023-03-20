eb5de0d641d9f560a671a186e83273c353a2862e
rony steve <rymel067@uottawa.ca>
2023-03-20 11:03:24 -0400

    2eme commit

--- jeu.js
+++ jeu.js
@@ -1,2 +1,12 @@
+const blocks = [...document.getElementsByClassName("block")];
+let joueur = document.getElementById("joueur");
+let nouveauJeu = document.getElementById("nouveauJeu");
+let joueurEnCours = 1;
 
+const jouerBlock = (e) => {
+  let idblock = e.target.id;
+
+  let req = {
+    id: parseInt(idblock)
+  };
 
